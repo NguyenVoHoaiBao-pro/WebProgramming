ec714b0f8e6f94c5e208311e752db5a76681a6bc
NguyenVoHoaiBao-pro <22130022@st.hcmuaf.edu.vn>
2024-11-07 23:50:03 +0700

    xu li su kien them vào gio hang

--- MartianChickenShop/html/index.js
+++ MartianChickenShop/html/index.js
@@ -2,9 +2,9 @@
 
 const productList = {
     "1": {
-        "name": "Yogi Leather Satchel",
-        "price": "4990",
-        "image": "/images/leather/products/58image.jpg"
+        "name": "Cánh gà kiểu Thái",
+        "price": ">50.000 đ",
+        "image": "/WebProgramming/MartianChickenShop/images/CanhGa/CanhGaKieuThai.png"
     },
     "2": {
         "name": "Wellesley Women's Laptop Bag",
@@ -110,63 +110,50 @@ const productList = {
 
 function addToCart() {
     let productId = window.location.href.split('/').pop().split('.')[0];
-    // productId = productId.charAt(productId.length - 1);
-    // if(productId.length == 9)
-    // {
-    //     productId = productId[8];
-    // }
-    // else if(productId.length == 10)
-    // {
-    //     productId = productId[8] + productId[9];
-    // }
     productId = productId.length == 9 ? productId[8] : productId[8] + productId[9];
-    let noi = document.getElementById('noi').value;
-    let cart = window.localStorage.getItem('cart');
-    if(cart == null) {
-        cart = {};
+    let noi = parseInt(document.getElementById('noi').value);
+    let cart = JSON.parse(window.localStorage.getItem('cart')) || {};
+
+    // Cập nhật số lượng nếu sản phẩm đã có trong giỏ hàng
+    if(cart[productId]) {
+        cart[productId] += noi;
     } else {
-        cart = JSON.parse(cart);
+        cart[productId] = noi;
     }
-    cart[productId] = noi;
+
     window.localStorage.setItem('cart', JSON.stringify(cart));
     alert('Sản phẩm đã được thêm vào giỏ hàng');
 }
 
 function removeFromCart(productId) {
-    let cart = window.localStorage.getItem('cart');
-    if(cart == null) {
-        cart = {};
-    } else {
-        cart = JSON.parse(cart);
+    let cart = JSON.parse(window.localStorage.getItem('cart')) || {};
+
+    if (cart[productId]) {
+        delete cart[productId];
+        window.localStorage.setItem('cart', JSON.stringify(cart));
+        window.location.reload();
     }
-    delete cart[productId];
-    window.localStorage.setItem('cart', JSON.stringify(cart));
-    window.location.reload();
 }
 
-function login() {
+function login(event) {
     event.preventDefault();
     let email = document.getElementById('email').value;
-
     alert('Logged in successfully as ' + email);
-
-    window.location.href = '/index.html';
+    window.location.href = '/WebProgramming/MartianChickenShop/html/index.html';
 }
 
-function contactUs() {
+function contactUs(event) {
     event.preventDefault();
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let message = document.getElementById('message').value;
-
     alert('Cảm ơn bạn đã liên hệ với chúng tôi. Chúng tôi sẽ liên hệ lại với bạn sớm nhất có thể.');
-
-    window.location.href = '/index.html';
+    window.location.href = '/WebProgramming/MartianChickenShop/html/index.html';
 }
 
-function signUp(){
+function signUp(event) {
     event.preventDefault();
     let email = document.getElementById('email').value;
     alert('Signed up successfully as ' + email);
-    window.location.href = '/Login.html';
+    window.location.href = '/WebProgramming/MartianChickenShop/html/Login.html';
 }
